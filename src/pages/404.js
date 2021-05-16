43638e614c935fc3780fc5e1b1106491c46c068a
Doruk <dorukde@gmail.com>
2021-05-16 21:27:53 +1000

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,8 +11,7 @@ const NotFoundPage = () => (
         <br />
         <p>Hello!</p>
         <p>
-          This is not the Lionel you're looking for! You have just found a page that
-          I have not designed yet. <br />
+          This is not the page you are looking for!<br />
           See you on the other side.
         </p>
       </div>
