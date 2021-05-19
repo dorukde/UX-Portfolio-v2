19f083d4d5bada2c974ba9835b5edb6f07335da0
Doruk <dorukde@gmail.com>
2021-05-19 23:28:07 +1000

    banners

--- src/pages/index2.js
+++ src/pages/index2.js
@@ -13,7 +13,12 @@ const IndexPage = props => (
   <div className="Content">
   <div id="rect">
   <Scroller />
-<Link to="/antler">
+
+  <Link to="/antler">
+<Img
+              fluid={props.data.aBanner.childImageSharp.fluid}
+              alt="Strafe"
+            />
     <FlexMenu
             flexOverline="PRODUCT DESIGN"
             flexTitle="ANTLER"
@@ -23,20 +28,77 @@ const IndexPage = props => (
     </Link>
 <br />
 <br />
+    <Link to="/strafe">
     <Img
-              fluid={props.data.aBanner.childImageSharp.fluid}
+              fluid={props.data.sBanner.childImageSharp.fluid}
               alt="Strafe"
             />
-    <Link to="/strafe">
     <FlexMenu
             flexOverline="UX-UI"
-            flexTitle="Strafe"
+            flexTitle="STRAFE"
             flexDetails="Strafe is the first esports tracking App for iOS and Android.
             Strafe provides a gamified experience with results, schedules,
             tournament info &amp; statistics for major esports titles."
             flexButton="CASE STUDY ⟶"
           />{' '}
     </Link>
+    <Link to="/keyflow">
+    <Img
+              fluid={props.data.kBanner.childImageSharp.fluid}
+              alt="Keyflow"
+            />
+    <FlexMenu
+            flexOverline="UX-UI"
+            flexTitle="KEYFLOW"
+            flexDetails="Keyflow, headquartered in Stockholm (SE), is an event services solution for both event producers and guests around the globe.
+            "
+            flexButton="CASE STUDY ⟶"
+          />{' '}
+    </Link>
+    <br />
+    <br /> 
+    <Link to="/keyflowbusiness">
+    <Img
+              fluid={props.data.kbBanner.childImageSharp.fluid}
+              alt="Keyflow Business"
+            />
+    <FlexMenu
+            flexOverline="UX-UI"
+            flexTitle="KEYFLOW BUSINESS"
+            flexDetails="Keyflow Business, is the solution for pre-event and real-time guest list management of each event of a nightclub. It features guest-list management, offline support, real-time request handling, considering the goals of venue producers and their teams."
+            flexButton="CASE STUDY ⟶"
+          />{' '}
+    </Link>
+    <br />
+    <br /> 
+    <Link to="/streetkak">
+    <Img
+              fluid={props.data.skBanner.childImageSharp.fluid}
+              alt="Streetkäk"
+            />
+    <FlexMenu
+            flexOverline="Product Design"
+            flexTitle="STREETKÄK"
+            flexDetails="Inspired by the growing street food culture in Scandinavia, and legendary Citroen H Van. Streetkäk is the first App to locate food trucks and other street food vendors in real time, in the Nordic countries such as Sweden, Denmark, Norway and Finland."
+            flexButton="CASE STUDY ⟶"
+          />{' '}
+    </Link>
+    <br />
+    <br /> 
+    <Link to="/otherworks">
+    <Img
+              fluid={props.data.ppBanner.childImageSharp.fluid}
+              alt="Personal Projects"
+            />
+    <FlexMenu
+            flexOverline="HCD, IxD, 3D, Print"
+            flexTitle="PERSONAL"
+            flexDetails="My personal design projects - including Interaction Design, Wearables, UI, Human Centred Product Design concept, 3D, Album Covers, Live Illustration and Print Design"
+            flexButton="CASE STUDY ⟶"
+          />{' '}
+    </Link>
+    <br />
+    <br /> 
   </div>
   </div>
   </div>
@@ -98,6 +160,47 @@ export const fluidImage480 = graphql`
 export const pageQuery = graphql`
   query {
     aBanner: file(relativePath: { eq: "antler_banner.jpg" }) {
+      ...fluidImage
+    }
+  }
+`
+export const pageQuery = graphql`
+  query {
+    sBanner: file(relativePath: { eq: "strafe_banner.jpg" }) {
+      ...fluidImage
+    }
+  }
+`
+
+export const pageQuery = graphql`
+  query {
+    kBanner: file(relativePath: { eq: "kf_banner.jpg" }) {
+      ...fluidImage
+    }
+  }
+`
+
+
+export const pageQuery = graphql`
+  query {
+    kbBanner: file(relativePath: { eq: "kf_business_banner.jpg" }) {
+      ...fluidImage
+    }
+  }
+`
+
+
+export const pageQuery = graphql`
+  query {
+    skBanner: file(relativePath: { eq: "streetkak_banner.jpg" }) {
+      ...fluidImage
+    }
+  }
+`
+
+export const pageQuery = graphql`
+  query {
+    ppBanner: file(relativePath: { eq: "personal_banner.jpg" }) {
       ...fluidImage
     }
   }
